d03a39af6973b49e0faae46ac33c94317cac6c8e
Bryan <Bresendizmedina@gmail.com>
2025-03-13 12:00:34 -0600

    Avance del arbol familiar

---
+++ Tareas-pl/ArbolGenealogico.pl
@@ -0,0 +1,55 @@
+padre(Salvador Grande).
+padre(Odilon)
+padre(Juan).
+padre(Salvador Mediano).
+padre(Rafael).
+padre(Miguel).
+padre(Alberto).
+padre(Samuel).
+
+Madre(Sara Grande).
+Madre(Rosalia).
+Madre(Guadalupe).
+Madre(Margarita).
+Madre(Yesenia).
+Madre(Sara).
+Madre(Marbe).
+Madre(Urisbi)
+Madre(Veronica).
+
+Hijo(Juan).
+Hijo(Alejandro).
+Hijo(Salvador Mediano).
+Hijo(Rafael).
+Hijo(Miguel).
+Hijo(Arturo).
+Hijo(Samuel).
+Hijo(Omar).
+Hijo(Juanito).
+Hijo(David).
+Hijo(Hector).
+Hijo(Bryan).
+Hijo(Aldair).
+Hijo(Rafita).
+Hijo(Miguelito).
+Hijo(Alex).
+Hijo(Emiliano).
+Hijo(Parros).
+Hijo(Kevin).
+
+Hija(Milagros).
+Hija(Regina).
+Hija(Perla).
+Hija(Wendy).
+Hija(Itari).
+Hija(Wereja).
+Hija(Yesica).
+Hija(Vanessa).
+Hija(Arely).
+Hija(Yarely).
+Hija(Alejandra).
+Hija(Kimberly).
+
+
+
+
